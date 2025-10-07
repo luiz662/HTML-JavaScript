7984a6926df43da4f7398086f67e741eaf891475
luiz662 <141882626+luiz662@users.noreply.github.com>
2025-10-06 21:12:34 -0300

    Add files via upload

---
+++ HTML-JavaScript-main/js/script13.js
@@ -0,0 +1,7 @@
+let idade = parseInt(prompt("digite a sua idade em anos ")) 
+if (idade<18){
+    alert("não pode entrar ")
+}else{
+    alert("pode entrar ")
+} 
+    
---
+++ HTML-JavaScript-main/praticandoJAVA/js/script02.js
@@ -0,0 +1,3 @@
+function gti(){
+    document.getElementById("demo") . innerHTML = "<h3>tablet</h3>"
+}
